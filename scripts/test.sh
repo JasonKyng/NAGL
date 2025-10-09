8399e5e220ca33c69e837bbf3ac51e6d53bf7ed2
JasonKyng <Yuexin.Wang23@student.xjtlu.edu.cn>
2025-10-09 10:35:46 +0800

    testing

---
+++ scripts/test.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+fold=$3
+
+if [ "$fold" == "0" ]; then
+    CUDA_VISIBLE_DEVICES=$1 python test.py \
+        --save_path ./outputs/train_on_visa \
+        --image_size 448 \
+        --dataset MVTec \
+        --n_shots 1 2 4 \
+        --a_shots 1 \
+        --num_learnable_proxies 25 \
+        --num_seeds 3 \
+        --eval_segm \
+        --tag default \
+        --data_root /gpfs/work/int/yuexinwang23/Datasets/mvtec
+else
+    if [ "$4" == "visa" ]; then
+        CUDA_VISIBLE_DEVICES=$1 python test.py \
+            --save_path $2 \
+            --image_size 448 \
+            --dataset VisA \
+            --n_shots 1 2 4 \
+            --a_shots 1 \
+            --num_learnable_proxies 25 \
+            --num_seeds 3 \
+            --eval_segm \
+            --tag default \
+            --data_root /path/to/dataset/visa
+    elif [ "$4" == "btad" ]; then
+        CUDA_VISIBLE_DEVICES=$1 python test.py \
+            --save_path $2 \
+            --image_size 448 \
+            --dataset BTAD \
+            --n_shots 1 2 4 \
+            --a_shots 1 \
+            --num_learnable_proxies 25 \
+            --num_seeds 3 \
+            --eval_segm \
+            --tag default \
+            --data_root /path/to/dataset/btad
+    elif [ "$4" == "brats" ]; then
+        CUDA_VISIBLE_DEVICES=$1 python test.py \
+            --save_path $2 \
+            --image_size 448 \
+            --dataset BraTS \
+            --n_shots 1 2 4 \
+            --a_shots 1 \
+            --num_learnable_proxies 25 \
+            --num_seeds 3 \
+            --eval_segm \
+            --tag default \
+            --data_root /path/to/dataset/brats
+    fi
+fi
